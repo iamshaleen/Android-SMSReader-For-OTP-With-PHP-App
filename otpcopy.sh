720edf325d80d539b4c9dc77b1a77c154ebc5870
Shaleen Jain <45964501+iamshaleen@users.noreply.github.com>
2023-06-09 16:39:26 +0800

    Update otpcopy.sh

--- otpcopy.sh
+++ otpcopy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true
 do
-        sshpass -p "<add password here>" rsync -acv va-user1@extlab:/var/www/html/POCMobileOTP/sow-files/* /var/www/html/sow-files/
+        sshpass -p "<add password here>" rsync -acv va-user1@lab:/var/www/html/POCMobileOTP/sow-files/* /var/www/html/sow-files/
 done
